9569e70b7f144bb4e1a9aa41ce8492094fb7c32b
Benjamin Bolbrinker <b.bolbrinker@gmail.com>
2018-12-12 16:25:35 +0100

    wip

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from disutils.core import setup
+
+setup(name='pyIETS',
+      version='0.0.1',
+      description='Post processing tool for SNF (MoViPac)',
+      author='Benjamin Bolbrinker',
+      author_email='benjamin.bolbrinker@chemie.uni-hamburg.de',
+      url='',
+      packages=[])
