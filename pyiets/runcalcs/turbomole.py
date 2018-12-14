8f2ca25470227ac96e26dbfc622390620458b81d
Benjamin Bolbrinker <b.bolbrinker@gmail.com>
2018-12-14 16:38:04 +0100

    Fixed folder problem

--- pyiets/runcalcs/turbomole.py
+++ pyiets/runcalcs/turbomole.py
@@ -6,13 +6,13 @@ from ase.calculators.turbomole import Turbomole
 
 
 def run(folder, paramdict):
-    #  cwd = os.getcwd()
-    #  print(cwd)
-    #  os.chdir(folder)
     print('Using turbomole...')
+    cwd = os.getcwd()
+    os.chdir(folder)
+
     calc = Turbomole(define_str=paramdict['sp_control']['define_string'])
 
-    files = glob.glob(folder + '/*')
+    files = glob.glob('*')
     if len(files) == 1:
         coord = files[0]
     elif len(files) == 0:
@@ -25,3 +25,5 @@ def run(folder, paramdict):
     molecule.set_calculator(calc)
 
     molecule.get_potential_energy()
+
+    os.chdir(cwd)
