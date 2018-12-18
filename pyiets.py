9df96753337d45f0361ce4c4e97954e35bf16e1c
Benjamin Bolbrinker <b.bolbrinker@gmail.com>
2018-12-18 13:30:47 +0100

    wip

--- pyiets.py
+++ pyiets.py
@@ -3,7 +3,6 @@
 
 import os
 import sys
-from glob import glob
 import pyiets.io.snfio
 import pyiets.io.createInput
 import pyiets.io.parseInput
@@ -14,12 +13,19 @@ if __name__ == '__main__':
     inparser = pyiets.io.parseInput.InputParser('input.json')
     options = inparser.getSinglePointOptions()
     qc_prog = options['sp_control']['qc_prog']
-    outfolder = pyiets.io.createInput.create_ascending_name('dissortions')
-    print(glob('./*/'))
-    if 'folder' in options:
-        outfolder = pyiets.io.createInput\
-                    .create_ascending_name(options['folder'])
 
-    pyiets.io.createInput.writeDisortion(outfolder, qc_prog,
-                                         'snf.out', delta=0.1)
-    calcmanager.start_tm_single_points_mp(outfolder, options, 6)
+    restart = False
+
+    if 'restart' in options:
+        restart = options['restart']
+
+    if restart:
+        folder =
+    else:
+        outfolder = pyiets.io.createInput.create_ascending_name('dissortions')
+        if 'folder' in options:
+            outfolder = pyiets.io.createInput \
+                        .create_ascending_name(options['folder'])
+        pyiets.io.createInput.writeDisortion(outfolder, qc_prog,
+                                             'snf.out', delta=0.1)
+        calcmanager.start_tm_single_points_mp(outfolder, options, 6)
