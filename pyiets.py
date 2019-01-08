2ae77dff84d62fd316470ce5e6938433b67c4300
Benjamin Bolbrinker <b.bolbrinker@gmail.com>
2019-01-08 15:42:23 +0100

    Lincense headers added.

--- pyiets.py
+++ pyiets.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# PyIETS
+
+# Postprocessing tool for calculating the IETS intensity and hence the
+# electron-phonon-interaction
+#
+# Copyright (C) 2019 Benjamin Bolbrinker, Michael Deffner,
+#                    Martin Zoellner, Carmen Herrmann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 import sys
 import pyiets.sp
@@ -19,6 +40,7 @@ def get_options(path):
 
 if __name__ == '__main__':
     workdir = sys.argv[1]
-    opt = get_options(workdir)
-    pyiets.sp.run(workdir, opt)
-    pyiets.artaios.run(workdir, opt)
+    options = get_options(workdir)
+    options['workdir'] = os.path.realpath(workdir)
+    pyiets.sp.run(workdir, options)
+    pyiets.artaios.run(workdir, options)
--- setup.py
+++ setup.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# PyIETS
+
+# Postprocessing tool for calculating the IETS intensity and hence the
+# electron-phonon-interaction
+#
+# Copyright (C) 2019 Benjamin Bolbrinker, Michael Deffner,
+#                    Martin Zoellner, Carmen Herrmann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from disutils.core import setup
 
 setup(name='pyIETS',
