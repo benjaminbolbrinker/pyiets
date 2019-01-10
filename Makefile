d4989bdf9e75b3c4966e666ce0d68b55810516f5
Benjamin Bolbrinker <b.bolbrinker@gmail.com>
2019-01-10 15:23:30 +0100

    wip

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 test:
-	./pyiets.py tests/sampleinput
+	make clean && ls -l /home/bolbrinker/Software/artaios/bin/ && ./pyiets.py tests/sampleinput
 
 clean: 
 	rm -rf ./tests/sampleinput/*dissortions* ./tests/sampleinput/*.restart
--- pyiets/io/artaios.py
+++ pyiets/io/artaios.py
@@ -1,4 +1,22 @@
+import re
+#  import numpy as np
+
+
+def readComplex(rawstr):
+    pass
+
+
 def readGreen(greenmatrixfile):
     with open(greenmatrixfile, 'r') as greenfile:
-        rawinput = greenfile.readlines()
-    print(rawinput)
+        rawinput = greenfile.readlines()[1:]
+
+    line = rawinput[0]
+    #  floating_point = r'[-+]?(\d+([.,]\d*)?|[.,]\d+)([eE][-+]?\d+)?'
+    floating_point = r'[-+]?\d+[.][Ee0-9+-]+'
+    #  print(line)
+    for line in rawinput:
+        ss = re.findall('[(] *' + floating_point + ', *' +
+                        floating_point + ' *[)]', line)
+
+        pass
+    #  print(rawinput)
