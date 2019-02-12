ba4d5e0b0a02c343da5c8bb388b396d1b54c5a72
Benjamin Bolbrinker <b.bolbrinker@gmail.com>
2019-02-12 10:46:31 +0100

    Changed Pipfile. For distribition python3-distuils required

--- setup.py
+++ setup.py
@@ -24,10 +24,13 @@
 
 from disutils.core import setup
 
-setup(name='pyIETS',
+setup(name='pyiets',
       version='0.0.1',
       description='Post processing tool for SNF (MoViPac)',
       author='Benjamin Bolbrinker',
       author_email='benjamin.bolbrinker@chemie.uni-hamburg.de',
+      license='GPLv3',
       url='',
-      packages=[])
+      packages=[],
+      scripts=['pyiets.py'],
+      zip_safe=False)
