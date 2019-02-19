weighted greenmatrices... units correct?

pyiets/atoms/molecule.py
@@ -28,7 +28,7 @@ class Molecule:
     """Small wrapper class to ASE-Atoms object.
 
     """
-    def __init__(self, atoms=None, vectors=None):
+    def __init__(self, atoms=None, atomicnumbers=None, vectors=None):
         """Creates molecule from atoms and vectors.
 
         Parameters
@@ -37,10 +37,13 @@ class Molecule:
             List declaring the atom names.
         vectors : :obj:`list` of :obj:`float`, optional
             List declaring vectors of atoms
+        atomicnumbers : :obj:`list` of :obj:`int`, optional
+            List declaring atomic numbers.
 
         """
         self.atoms = atoms
         self.vectors = np.array(vectors)
+        self.an = atomicnumbers
         for idx, at in enumerate(self.atoms):
             self.atoms[idx] = self.atoms[idx][0] + self.atoms[idx][1:].lower()
 
@@ -60,6 +63,6 @@ class Molecule:
 
         """
         print('Printing molecule')
-        assert len(self.atoms) == len(self.vectors)
+        assert len(self.atoms) == len(self.vectors) == len(self.an)
         for idx, atom in enumerate(self.atoms):
-            print(atom, self.vectors[idx])
+            print(atom, self.an[idx], self.vectors[idx])

pyiets/troisi.py
@@ -12,7 +12,7 @@ class Troisi:
     ''' Class for calculating the IETS intensity from previous single
     point and transport calculations.
     '''
-    def __init__(self, options, modes, greenmat_dictarr):
+    def __init__(self, options, modes, greenmat_dictarr, molecule):
         """ Constructor of SinglePoint class.
         Note
         ----
@@ -38,6 +38,7 @@ class Troisi:
         self.options = options
         self.modes = modes
         self.greenmat_dictarr = greenmat_dictarr
+        self.molecule = molecule
         self.troisi_greenmatrices = []
         self.output_mode_folders = [None]*len(modes)
         self.iets_dict_list = None
@@ -64,12 +65,18 @@ class Troisi:
         gm_idx = [[g for g in self.greenmat_dictarr
                   if mode.get_folders()[idx] == g['mode']][0]
                   for idx in range(len(mode.get_folders()))]
-        [print(mode.atoms) for mode in self.modes]
         d0 = self.options['delta']
+        reduced_mass = 0.0
+        assert len(mode.get_vectors()) == len(self.molecule.an)
+        for idx, vector in enumerate(mode.get_vectors()):
+            for coord in vector:
+                reduced_mass += (coord**2)/self.molecule.an[idx]
 
+        reduced_mass = 1.0 / reduced_mass
         troisi_greenmatrix = (math.sqrt(2*d0)/(2*d0) * (0.5*d0/cstep) *
                               (np.array(gm_idx[1]['greensmatrix'])
                               - np.array(gm_idx[0]['greensmatrix'])))
+        troisi_greenmatrix /= math.sqrt(reduced_mass)
         mode.set_troisi_greensmat(gm=troisi_greenmatrix)
 
     def _init_output(self, path_to_sp):