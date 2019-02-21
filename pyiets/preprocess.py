5404b6249527fe4c354271292d01eafa317e2ae0
Benjamin Bolbrinker <b.bolbrinker@gmail.com>
2019-02-21 17:11:15 +0100

    wip

--- pyiets/preprocess.py
+++ pyiets/preprocess.py
@@ -30,9 +30,9 @@ class Preprocessor():
             modes = [self.parser.get_mode(int(mode_idx))
                      for mode_idx in modes]
 
-        [mode.print() for mode in modes]
+        print('Unweighting mode vectors...')
         [mode.to_non_weighted() for mode in modes]
-        [mode.print() for mode in modes]
+        print('Done')
         if self.options['restart']:
             return (self._prepareDistortions(modes),
                     self.parser.get_molecule())
