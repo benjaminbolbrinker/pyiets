ae0675e7c5c902fe4286bc821b6710a75a20ec1b
Benjamin Bolbrinker <b.bolbrinker@gmail.com>
2019-04-18 12:56:50 +0200

    wip

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Benjamin Bolbrinker, Michael Deffner, Martin Zoellner, Carmen
 author = 'Benjamin Bolbrinker, Michael Deffner, Martin Zoellner, Carmen Herrmann'
 
 # The short X.Y version
-version = '0.1.1'
+version = '0.1.3'
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.3'
 
 
 # -- General configuration ---------------------------------------------------
