b58bb8e68ea7cba158ee5e5b23677f758a8fb7f2
Benjamin Bolbrinker <b.bolbrinker@gmail.com>
2019-04-29 16:27:43 +0200

    Rewriting a bit

---
+++ pyiets/runcalcs/tm2unformsoc.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+
+
+def run(params):
+    """Run artaios preprocess script in specified folder.
+
+    Parameters
+    ----------
+        params : :obj:`list`
+            folder : obj`str`
+                name of folder to perform calculation in.
+            options : :obj`dict`
+                Options.
+    """
+
+    folder = params[0]
+    options = params[1]
+
+    cwd = os.getcwd()
+    # subprocess.call(['cp', os.path.realpath(options['artaios_in']), folder])
+    os.chdir(folder)
+
+    stdoutname = 'artaios_tm_soc.stdout'
+    stderrname = 'artaios_tm_soc.stderr'
+    if options['verbose']:
+        print('''
+Starting tm2unformsoc in \'{}\'
+Redirecting output to \'{}\' and \'{}\'
+'''.format(folder, stdoutname, stderrname))
+
+    # Run and redirect output
+    process = subprocess.Popen(options['artaios_tm_soc_bin'],
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    with open(stdoutname, 'wb') as f:
+        for line in process.stdout:
+            f.write(line)
+
+    with open(stderrname, 'wb') as f:
+        for line in process.stderr:
+            f.write(line)
+
+    os.chdir(cwd)
---
+++ tests/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import filecmp
+
+
+test_dirs = ['C10H4Au6S2_dscf',
+             'C10H4Au6S2_ridft',
+             'H2O_dscf',
+             'H2O_dscf_gaussian',
+             'H2O_dscf_tm_parms',
+             'H2O_M',
+             'H2O_ridft',
+             'H2O_ridft_fakegaussianin',
+             'H2O_ridft_gaussianin',
+             'H2O_ridft_tm_params',
+             'H2O_ridft_turbomolein_M',
+             'H2O_self']
+
+for test in test_dirs:
+    if filecmp.cmp(os.path.join(test, 'iets.dat'),
+                   os.path.join(test, 'test.dat')):
+        print(test + ' - OK')
+    else:
+        print(test + ' - NOT OK. Terminating...')
+        sys.exit()
