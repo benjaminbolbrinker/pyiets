5ce9317bd21293d4b548a39013d06045ca8b2e88
Benjamin Bolbrinker <b.bolbrinker@gmail.com>
2019-05-10 10:59:42 +0200

    wip

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 pip3 uninstall pyiets
-rm $HOME/.local/bin/pyiets $HOME/.local/bin/pyiets-plot $HOME/.local/bin/pyiets-compplot $HOME/.local/bin/pyiets-clean $HOME/.local/bin/pyiets-gaussian-broadening
+rm $HOME/.local/bin/pyiets $HOME/.local/bin/pyiets-plot $HOME/.local/bin/pyiets-compplot $HOME/.local/bin/pyiets-clean $HOME/.local/bin/pyiets-gaussian-broadening $HOME/.local/bin/pyiets-plot-spindiff $HOME/.local/bin/pyiets-plot-spindiff-gaussian
